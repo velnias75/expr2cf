4d4bfc9356ededdff4e5531984e926d3b51c15b2
Heiko Schaefer <heiko@rangun.de>
2015-11-01 00:30:47 +0100

    added repeated decimal to fraction tool

---
+++ src/rd2frac.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of expr2cf.
+ *
+ * expr2cf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * expr2cf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with expr2cf.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <iterator>
+#include <cstdlib>
+#include <sstream>
+#include <ios>
+
+#include <rational/gmp_rational.h>
+
+int main(int argc, const char *argv[]) {
+
+	mpf_set_default_prec(65536);
+
+#ifdef HAVE_MPREAL_H
+	mpfr_set_default_prec(std::min<mpfr_prec_t>(65536, MPFR_PREC_MAX));
+#endif
+
+	try {
+
+		const Commons::Math::gmp_rational::integer_type x(argc > 1 ? argv[1] : "0");
+		const Commons::Math::gmp_rational::integer_type pre(argc > 3 ? argv[3] : "0");
+		const std::size_t lz(argc > 2 ? static_cast<std::size_t>(std::atol(argv[2])) : 0u);
+		const std::size_t plz(argc > 4 ? static_cast<std::size_t>(std::atol(argv[4])) : 0u);
+
+		std::cout << Commons::Math::rf<mpz_class, Commons::Math::GCD_gmp,
+			Commons::Math::NO_OPERATOR_CHECK>(x, lz, pre, plz) << std::endl;
+
+	} catch(const std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
