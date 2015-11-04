aff7496ea95802fa19e8d142399795da58acefc0
Heiko Schaefer <heiko@rangun.de>
2015-11-04 16:33:56 +0100

    added expr2rd

---
+++ src/expr2rd.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of expr2cf.
+ *
+ * expr2cf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * expr2cf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with expr2cf.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <iterator>
+
+#include <rational/gmp_rational.h>
+
+int main(int, const char *[]) {
+
+	mpf_set_default_prec(65536);
+
+#ifdef HAVE_MPREAL_H
+	mpfr_set_default_prec(std::min<mpfr_prec_t>(65536, MPFR_PREC_MAX));
+#endif
+
+	try {
+
+		Commons::Math::gmp_rational r;
+
+		std::cin >> std::noskipws >> r;
+
+		Commons::Math::gmp_rational::rf_info i;
+
+		std::cout << "[ " << r.decompose(i) << ", ";
+
+		if(!i.reptent_digits.empty()) {
+
+			std::copy(i.reptent_digits.begin(), i.reptent_digits.end(),
+				std::ostream_iterator<Commons::Math::gmp_rational::integer_type>(std::cout));
+
+		} else {
+			std::cout << "0";
+		}
+
+		std::cout << ", ";
+
+		if(!i.pre_digits.empty()) {
+
+			std::copy(i.pre_digits.begin(), i.pre_digits.end(),
+				std::ostream_iterator<Commons::Math::gmp_rational::integer_type>(std::cout));
+
+		} else {
+			std::cout << "0";
+		}
+
+		std::cout << " ]" << std::endl;
+
+	} catch(const std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
