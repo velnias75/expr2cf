24c10b6008a559e38a5cccd7f3d4ad06ddc038ad
velnias75 <heiko@rangun.de>
2016-01-02 08:10:19 +0100

    added missing exprsqrt.cpp

---
+++ src/exprsqrt.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of expr2cf.
+ *
+ * expr2cf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * expr2cf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with expr2cf.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include <rational/gmp_rational.h>
+
+int main ( int argc, const char * argv[] ) {
+
+    mpf_set_default_prec ( 65536 );
+
+#ifdef HAVE_MPREAL_H
+    mpfr_set_default_prec ( std::min<mpfr_prec_t> ( 65536, MPFR_PREC_MAX ) );
+#endif
+
+    try {
+
+        Commons::Math::gmp_rational r;
+		
+        std::cin >> std::noskipws >> r;
+        std::cout << r.sqrt() << std::endl;
+
+    } catch ( const std::exception &e ) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+// kate: indent-mode cstyle; indent-width 4; replace-tabs on;
