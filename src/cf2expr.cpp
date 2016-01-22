d50a77965fdf7336e69459d9a66125949cf76efa
Heiko Schaefer <heiko@rangun.de>
2016-01-22 07:50:46 +0100

    cin.imbue() needs -frtti

--- src/cf2expr.cpp
+++ src/cf2expr.cpp
@@ -24,7 +24,7 @@
 
 struct cf_reader : std::ctype<char> {
 
-    inline cf_reader() : std::ctype<char> ( get_table() ) {}
+    inline cf_reader(std::size_t refs = 0) : std::ctype<char>(get_table(), false, refs) {}
 
     static std::ctype_base::mask const *get_table() {
 
@@ -46,7 +46,7 @@ int main ( int, const char *[] ) {
 
     try {
 
-        std::cin.imbue ( std::locale ( std::locale(), new cf_reader() ) );
+        std::cin.imbue ( std::locale ( std::locale(), new cf_reader ) );
 
         std::cout << Commons::Math::cf (
                       std::istream_iterator<Commons::Math::gmp_rational::integer_type> ( std::cin ),
